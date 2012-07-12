1a66f1e2edd80e99f28f2107ed95bc6ced83baaf
Mike McMullan <apps@mike-mcmullan.com>
2012-07-11 21:50:07 -0400

    Initial Commit.

---
+++ Gemfile
@@ -0,0 +1,3 @@
+source "http://rubygems.org"
+
+gem "mechanize", "~> 2.5.1"
---
+++ example.config.rb
@@ -0,0 +1,4 @@
+# Rename this file to config.rb and set your icoke email and password to start entering pins.
+@email		= 'icoke-username'
+@password	= 'icoke-password'
+@cookie_file= './icoke_cookie.txt' # The location where the cookie file will be stored, default: current directory.
